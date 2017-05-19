7ff6238ebc3ad03964f4af0ff33af38b827ca34e
vedanta-krit.das <vedmalex@gmail.com>
2017-05-19 12:59:44 +0300

    fix: viewer

--- generators/app/templates/src/model/connectors.ts
+++ generators/app/templates/src/model/connectors.ts
@@ -6,8 +6,7 @@ export default class DataConnectors extends RegisterConnectors {
   public get Viewer(): User {
     if (!this._Viewer) {
       this._Viewer = new User({ mongoose: this.mongoose, connectors: this, user: this.user, owner: this.owner, acls: this.acls, userGroup: this.userGroup });
-    };
-  }
+    }
     return this._Viewer;
   }
   protected _Viewer: User;
