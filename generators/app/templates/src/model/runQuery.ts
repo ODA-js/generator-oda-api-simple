6d30c5b98a6e2b3d57416ff4b6a0fca3a4dfa72c
vedanta-krit.das <vedmalex@gmail.com>
2017-05-25 11:59:17 +0300

    fix: possible cyclic require when using api-hooks with queries.

--- generators/app/templates/src/model/runQuery.ts
+++ generators/app/templates/src/model/runQuery.ts
@@ -8,23 +8,28 @@ import { SystemSchema } from '../model/schema';
 import { ExecutionResult } from 'graphql';
 import { runQuery } from 'graphql-server-core';
 
-let schemas = new SystemSchema({});
+let schemas = () => new SystemSchema({});
 
 let pool = new DbMongooseConnectionPool({ defaultUrl: config.get<string>('db.api.url') });
 
 export class SystemGraphQL {
-
+  private static _schemas;
+  private static _schema;
   private static schema = (() => {
-    let result;
-    schemas.build();
-    result = makeExecutableSchema({
-      typeDefs: schemas.typeDefs.toString(),
-      resolvers: schemas.resolvers,
-      resolverValidationOptions: {
-        requireResolversForNonScalar: false,
-      },
-    });
-    return result;
+    if (!SystemGraphQL._schemas) {
+      SystemGraphQL._schemas = schemas();
+    }
+    if (!SystemGraphQL._schema) {
+      SystemGraphQL._schemas.build();
+      SystemGraphQL._schema = makeExecutableSchema({
+        typeDefs: SystemGraphQL._schemas.typeDefs.toString(),
+        resolvers: SystemGraphQL._schemas.resolvers,
+        resolverValidationOptions: {
+          requireResolversForNonScalar: false,
+        },
+      });
+    }
+    return SystemGraphQL._schema;
   })();
 
   public static async connectors() {
@@ -46,7 +51,7 @@ export class SystemGraphQL {
     return await runQuery({
       query,
       variables,
-      schema: SystemGraphQL.schema,
+      schema: SystemGraphQL.schema(),
       context: await SystemGraphQL.context(),
     });
   }
